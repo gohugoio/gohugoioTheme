966215fbb2ec306f3bc550668963bcb52f498e22
Bud Parr <budparr@gmail.com>
2017-04-27 23:46:27 -0400

    style code blocks #1

--- src/js/hljs.js
+++ src/js/hljs.js
@@ -9,6 +9,7 @@ hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascri
 hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
 hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('html', require('highlight.js/lib/languages/handlebars'));
 
 hljs.registerLanguage("go", function(e) {
   var t = { keyword: "code output note warning break default func interface select case map struct chan else goto package switch const fallthrough if range end type continue for import return var go defer bool byte complex64 complex128 float32 float64 int8 int16 int32 int64 string uint8 uint16 uint32 uint64 int uint uintptr rune id autoplay Get", literal: "file download copy true false iota nil Pages with", built_in: "append cap close complex highlight copy imag len make new panic print println real recover delete Site Data tweet speakerdeck youtube ref relref vimeo instagram gist figure innershortcode" };
