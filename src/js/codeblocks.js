18141c496f18ac74bfef480403b4bbb69c44bbbe
budparr <budparr@gmail.com>
2017-07-17 15:09:35 -0400

    Fix object iterator in codeblock
    Error: Uncaught TypeError: myObject[Symbol.iterator] is not a function
    
    Found a fix for the issue here: https://github.com/babel/babel-loader/issues/84
    
    Fixes #84
    
    Signed-off-by: budparr <budparr@gmail.com>

--- src/js/codeblocks.js
+++ src/js/codeblocks.js
@@ -2,8 +2,7 @@ let article = document.getElementById('prose')
 
 if (article) {
   let codeBlocks = article.getElementsByTagName('code')
-
-  for (let codeBlock of codeBlocks) {
+    for (let [key, codeBlock] of Object.entries(codeBlocks)){
     var widthDif = codeBlock.scrollWidth - codeBlock.clientWidth
     if (widthDif > 0)
       codeBlock.parentNode.classList.add('expand')
